1db3286673daba213c06e9a04f378d4d97e5d8ed
lymacasm <lyden.m.smith@gmail.com>
2017-06-13 15:49:15 -0700

    Added mag support

--- src/imu.py
+++ src/imu.py
@@ -9,7 +9,7 @@ def twos_complement(val, bits):
 class IMU:
     def __init__(self):
         self.bus = SMBus(1) # initialize I2C bus on I2C interface 1
-        self._slave_addr_ = 0x68
+        self._imu_addr_ = 0x68
         self._accel_config_ = 0x1C
         self._accel_limits_ = [2, 4, 8, 16]
         self._accel_x_ = 0x3B
@@ -21,56 +21,95 @@ class IMU:
         self._gyro_x_ = 0x43
         self._gyro_y_ = 0x45
         self._gyro_z_ = 0x47
+        self._int_bypass_ = 0x37
+        self._mag_addr_ = 0x0C
         self._mag_x_ = 0x03
         self._mag_y_ = 0x05
         self._mag_z_ = 0x07
+        self._mag_st2_ = 0x09
+        self._mag_cntl1_ = 0x0A
+
+        # Enable bypass so we can talk to magnetometer
+        bypass_config = self.bus.read_byte_data(self._imu_addr_, self._int_bypass_)
+        bypass_config = bypass_config | 0x2
+        self.bus.write_byte_data(self._imu_addr_, self._int_bypass_, bypass_config)
+
+        # Configure magnetometer in continuous mode
+        mag_config = self.bus.read_byte_data(self._mag_addr_, self._mag_cntl1_)
+        mag_config = mag_config | 0x12 # 16 bit, Continuous mode 1
+        self.bus.write_byte_data(self._mag_addr_, self._mag_cntl1_, mag_config)
+
 
     def getAccelX(self):
         # Returns raw x-component from accelerometer
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_x_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_x_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_x_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_x_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
     def getAccelY(self):
         # Returns raw y-component from accelerometer
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_y_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_y_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_y_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_y_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
     def getAccelZ(self):
         # Returns raw z-component from accelerometer
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_z_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._accel_z_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_z_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._accel_z_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
     def getGyroX(self):
         # Returns raw x-component from gyroscope
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_x_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_x_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_x_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_x_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
     def getGyroY(self):
         # Returns raw y-component from gyroscope
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_y_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_y_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_y_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_y_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
     def getGyroZ(self):
         # Returns raw z-component from gyroscope
-        high_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_z_)
-        low_byte = self.bus.read_byte_data(self._slave_addr_, self._gyro_z_ + 1)
+        high_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_z_)
+        low_byte = self.bus.read_byte_data(self._imu_addr_, self._gyro_z_ + 1)
         data_raw = (high_byte << 8) | low_byte
         return twos_complement(data_raw, 16)
 
+    def getMagX(self):
+        # Returns raw x-component from magnetometer
+        low_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_x_)
+        high_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_x_ + 1)
+        overflow = self.bus.read_byte_data(self._mag_addr_, self._mag_st2_)
+        data_raw = (high_byte << 8) | low_byte
+        return twos_complement(data_raw, 16)
+
+    def getMagY(self):
+        # Returns raw y-component from magnetometer
+        low_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_y_)
+        high_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_y_ + 1)
+        overflow = self.bus.read_byte_data(self._mag_addr_, self._mag_st2_)
+        data_raw = (high_byte << 8) | low_byte
+        return twos_complement(data_raw, 16)
+
+    def getMagZ(self):
+        # Returns raw z-component from magnetometer
+        low_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_z_)
+        high_byte = self.bus.read_byte_data(self._mag_addr_, self._mag_z_ + 1)
+        overflow = self.bus.read_byte_data(self._mag_addr_, self._mag_st2_)
+        data_raw = (high_byte << 8) | low_byte
+        return twos_complement(data_raw, 16)
+        
     def getTemp(self):
         # Returns raw temperature
-        return (self.bus.read_byte_data(self._slave_addr_, self._temp_) << 8) | \
-               self.bus.read_byte_data(self._slave_addr_, self._temp_ + 1)
+        return (self.bus.read_byte_data(self._imu_addr_, self._temp_) << 8) | \
+               self.bus.read_byte_data(self._imu_addr_, self._temp_ + 1)
 
     def getAccel(self):
         # Returns dictionary with raw accelerometer readings in each direction
@@ -80,27 +119,35 @@ class IMU:
         # Returns dictionary with raw gyroscope readings in each direction
         return {'x':self.getGyroX(), 'y':self.getGyroY(), 'z':self.getGyroZ()}
 
+    def getMag(self):
+        # Returns dictionary with raw magnetometer readings in each direction
+        return {'x':self.getMagX(), 'y':self.getMagY(), 'z':self.getMagZ()}
+
     def getAccelRangeG(self):
         # Returns current setting for max value in g's (min is negative max)
-        config_reg = self.bus.read_byte_data(self._slave_addr_, self._accel_config_)
+        config_reg = self.bus.read_byte_data(self._imu_addr_, self._accel_config_)
         config_reg = (config_reg & 0x18) >> 3
         return self._accel_limits_[config_reg]
 
     def getGyroRangeDps(self):
         # Returns current setting for max value in dps (min is negative max)
-        config_reg = self.bus.read_byte_data(self._slave_addr_, self._gyro_config_)
+        config_reg = self.bus.read_byte_data(self._imu_addr_, self._gyro_config_)
         config_reg = (config_reg & 0x18) >> 3
         return self._gyro_limits_[config_reg]
+
+    def getMagRangeuT(self):
+        # Non-configurable, just for consistency and conversions
+        return 4912
 
     def setAccelRangeG(self, limit):
         # Sets the max accel value in g's (min is negative max)
         if limit not in self._accel_limits_:
             raise ValueError("Invalid value for limit argument. Must be one of %s" % self._accel_limits_)
 
-        config_reg = self.bus.read_byte_data(self._slave_addr_, self._accel_config_)
+        config_reg = self.bus.read_byte_data(self._imu_addr_, self._accel_config_)
         config_val = self._accel_limits_.index(limit)
         config_reg = (config_reg & ~0x18) | ((config_val & 0x3) << 3)
-        self.bus.write_byte_data(self._slave_addr_, self._accel_config_, config_reg)
+        self.bus.write_byte_data(self._imu_addr_, self._accel_config_, config_reg)
         return
 
     def setGyroRangeDps(self, limit):
@@ -108,10 +155,10 @@ class IMU:
         if limit not in self._gyro_limits_:
             raise ValueError("Invalid value for limit argument. Must be one of %s" % self._gyro_limits_)
 
-        config_reg = self.bus.read_byte_data(self._slave_addr_, self._gyro_config_)
+        config_reg = self.bus.read_byte_data(self._imu_addr_, self._gyro_config_)
         config_val = self._gyro_limits_.index(limit)
         config_reg = (config_reg & ~0x18) | ((config_val & 0x3) << 3)
-        self.bus.write_byte_data(self._slave_addr_, self._gyro_config_, config_reg)
+        self.bus.write_byte_data(self._imu_addr_, self._gyro_config_, config_reg)
         return
 
     def getAccelXG(self):
@@ -150,6 +197,24 @@ class IMU:
         limit = float(self.getGyroRangeDps())
         return val * limit / 0x7FFF
 
+    def getMagXuT(self):
+        # Returns x-component from magnetometer in uT
+        val = float(self.getMagX())
+        limit = float(self.getMagRangeuT())
+        return val * limit / 0x7FF8
+
+    def getMagYuT(self):
+        # Returns y-component from magnetometer in uT
+        val = float(self.getMagY())
+        limit = float(self.getMagRangeuT())
+        return val * limit / 0x7FF8
+
+    def getMagZuT(self):
+        # Returns y-component from magnetometer in uT
+        val = float(self.getMagZ())
+        limit = float(self.getMagRangeuT())
+        return val * limit / 0x7FF8
+
     def getAccelG(self):
         # Returns accelerometer data in all directions in g's
         return {'x':self.getAccelXG(), 'y':self.getAccelYG(), 'z':self.getAccelZG()}
@@ -157,6 +222,10 @@ class IMU:
     def getGyroDps(self):
         # Returns gyroscope data in all directions in dps
         return {'x':self.getGyroXDps(), 'y':self.getGyroYDps(), 'z':self.getGyroZDps()}
+
+    def getMaguT(self):
+        # Returns magnetometer data in all directions in uT
+        return {'x':self.getMagXuT(), 'y':self.getMagYuT(), 'z':self.getMagZuT()}
 
     def convertAccelToG(self, raw, limit):
         # Converts raw accelerometer data to g's. User is
@@ -171,3 +240,8 @@ class IMU:
         if limit not in self._gyro_limits_:
             raise ValueError("Limit must be one of the following: %s" % self._gyro_limits_)
         return float(raw) * float(limit) / 0x7FFF
+
+    def convertMagTouT(self, raw):
+        # Converts raw magnetometer data to uT
+        limit = float(self.getMagRangeuT())
+        return float(raw) * limit / 0x7FF8
